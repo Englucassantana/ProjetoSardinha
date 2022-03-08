b25b2094536f9a16357315c1f924b2e3b18c4b48
Lucas Santana <eng.lucassantana@gmail.com>
2022-03-07 21:47:16 -0300

    Remoçao de arquivos sem utilizaçao

--- Teste/returnevent/returnevent.js
+++
@@ -1,9 +0,0 @@
-let xhr = new XMLHttpRequest();
-
-xhr.addEventListener('load', function (event,par){
-
-});
-
-xhr.open('GET',"https://api.binance.com/api/v3/ticker/price", true);
-// xhr.open('GET',"https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT", true);
-xhr.send(null);
